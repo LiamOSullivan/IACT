2b0f53e6092046ea556856c7a6bd3ac13ca0fdfa
mmtadmin <mmtadmin@pc97.mee.tcd.ie>
2016-11-25 17:03:42 +0000

    structuring with js files and html

---
+++ IACT_Foundations/public_html/01_GettingStarted/01_01_helloWorld/01_01_helloWorld.js
@@ -0,0 +1,17 @@
+/*************************
+ * Foundations in Coding *       
+ *************************
+ *  IACT ** www.iact.ie  *
+ *************************/
+
+/**************************************
+* Hello World
+***************************************
+Prints the common 'Hello World' phrase 
+to the console window.
+***************************************/
+
+//Print to the browser console
+console.log("Hello World!"); 
+//Display text in the browser
+text("Hello World!");
